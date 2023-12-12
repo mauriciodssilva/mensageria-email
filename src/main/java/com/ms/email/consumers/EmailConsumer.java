f5a37ceb10da3dd9263d96a635d5352e24e8cde9
Mauricio dos Santos <43189404+mauriciodssilva@users.noreply.github.com>
2023-12-11 22:15:19 -0300

    Adição do Projeto

---
+++ src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -0,0 +1,19 @@
+
+package com.ms.email.consumers;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Mauricio Silva
+ */
+@Component
+public class EmailConsumer {
+
+    @RabbitListener(queues = "${broker.queue.email.name}")
+    public void listenEmailQueue(@Payload String string) {
+        System.out.println(string);
+    }
+}
